8630502801f4b36b86ce7697dc843993daedbb1b
Samir Gupta <sg8765@srmist.edu.in>
2026-02-16 08:54:25 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Samir
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method – entry point of Java application
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+
+    }
+}
