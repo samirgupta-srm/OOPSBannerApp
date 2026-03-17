67f93a3476e9e17f7d4fae3976a1fe63411a7e5b
Samir Gupta <sg8765@srmist.edu.in>
2026-03-17 08:52:15 +0530

    UC4 - Array and Loops Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,19 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("WELCOME TO OOPS BANNER APP");
+
+        // Store banner lines inside a String array
+        String[] banner = {
+            String.join("  ", "W   W", "EEEEE", "L    ", "C   C", " OOO ", "M   M", "EEEEE"),
+            String.join("  ", "W   W", "E    ", "L    ", "C    ", "O   O", "MM MM", "E    "),
+            String.join("  ", "W W W", "EEEE ", "L    ", "C    ", "O   O", "M M M", "EEEE "),
+            String.join("  ", "WW WW", "E    ", "L    ", "C    ", "O   O", "M   M", "E    "),
+            String.join("  ", "W   W", "EEEEE", "LLLLL", "C   C", " OOO ", "M   M", "EEEEE")
+        };
+
+        // Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
