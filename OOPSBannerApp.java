25c3cf59d1899f99ca90c9924aad0e4a9d57425e
Samir Gupta <sg8765@srmist.edu.in>
2026-03-17 09:33:00 +0530

    UC6 - Static functions for banner letters

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,80 @@
 public class OOPSBannerApp {
 
+    // Static methods for each letter
+    static String[] getW() {
+        return new String[]{
+            "W   W",
+            "W   W",
+            "W W W",
+            "WW WW",
+            "W   W"
+        };
+    }
+
+    static String[] getE() {
+        return new String[]{
+            "EEEEE",
+            "E    ",
+            "EEEE ",
+            "E    ",
+            "EEEEE"
+        };
+    }
+
+    static String[] getL() {
+        return new String[]{
+            "L    ",
+            "L    ",
+            "L    ",
+            "L    ",
+            "LLLLL"
+        };
+    }
+
+    static String[] getC() {
+        return new String[]{
+            "C   C",
+            "C    ",
+            "C    ",
+            "C    ",
+            "C   C"
+        };
+    }
+
+    static String[] getO() {
+        return new String[]{
+            " OOO ",
+            "O   O",
+            "O   O",
+            "O   O",
+            " OOO "
+        };
+    }
+
+    static String[] getM() {
+        return new String[]{
+            "M   M",
+            "MM MM",
+            "M M M",
+            "M   M",
+            "M   M"
+        };
+    }
+
     public static void main(String[] args) {
 
-        // Array declaration + initialization together (UC5)
-        String[] banner = {
-            String.join("  ", "W   W", "EEEEE", "L    ", "C   C", " OOO ", "M   M", "EEEEE"),
-            String.join("  ", "W   W", "E    ", "L    ", "C    ", "O   O", "MM MM", "E    "),
-            String.join("  ", "W W W", "EEEE ", "L    ", "C    ", "O   O", "M M M", "EEEE "),
-            String.join("  ", "WW WW", "E    ", "L    ", "C    ", "O   O", "M   M", "E    "),
-            String.join("  ", "W   W", "EEEEE", "LLLLL", "C   C", " OOO ", "M   M", "EEEEE")
-        };
+        String[] w = getW();
+        String[] e = getE();
+        String[] l = getL();
+        String[] c = getC();
+        String[] o = getO();
+        String[] m = getM();
 
-        // Loop (same as UC4)
-        for (String line : banner) {
-            System.out.println(line);
+        // Build banner using loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println(
+                String.join("  ", w[i], e[i], l[i], c[i], o[i], m[i], e[i])
+            );
         }
     }
 }
