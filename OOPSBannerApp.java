475ab5f65915e271f29de915b90556703dbdb71f
Samir Gupta <sg8765@srmist.edu.in>
2026-03-17 09:27:07 +0530

    UC5 - Array Initialization using String.join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,7 +2,7 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Store banner lines inside a String array
+        // Array declaration + initialization together (UC5)
         String[] banner = {
             String.join("  ", "W   W", "EEEEE", "L    ", "C   C", " OOO ", "M   M", "EEEEE"),
             String.join("  ", "W   W", "E    ", "L    ", "C    ", "O   O", "MM MM", "E    "),
@@ -11,7 +11,7 @@ public class OOPSBannerApp {
             String.join("  ", "W   W", "EEEEE", "LLLLL", "C   C", " OOO ", "M   M", "EEEEE")
         };
 
-        // Loop to print banner
+        // Loop (same as UC4)
         for (String line : banner) {
             System.out.println(line);
         }
